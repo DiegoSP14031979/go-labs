8219c3fc1b3b73976a65ce06fdfdfc74bd6e9cd6
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-18 16:07:09 +0200

    comments in GO
    Added single-line and multi-line comments to the main function.

---
+++ 01-basics/03_comments.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Score: 10")
+
+    // Turn the line below into a comment so that it stops executing
+    //fmt.Println("Loading...")
+
+    // Turn the two lines below into a multi-line comment so neither executes
+    /*fmt.Println("Debug start")
+    fmt.Println("Debug end")*/
+}
