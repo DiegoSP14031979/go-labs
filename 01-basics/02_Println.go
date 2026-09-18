6bcf4be9ee6e9f64adbab8ce0a4713211374f1a6
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-18 16:01:19 +0200

    Update comments and print statement in 02_Println.go

---
+++ 01-basics/02_Println.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// I use fmt.Println() to print "Alex 25 coding" in a unique line
+	// You can provide multiple arguments to fmt.Println()
+	fmt.Println("Alex", "25", "coding")
+}
