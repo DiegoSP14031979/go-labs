bfbb8956d342c0114f3aa93150c389cd806c8860
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-18 16:12:37 +0200

    integer variable examples
    Added integer variables and print statements.

---
+++ 02-variables/03_int.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Crea tus variables enteras aquí
+	age := 25
+	year := 2023
+	temperature := -5
+	
+	// Imprime las variables en líneas separadas
+	fmt.Println(age)
+	fmt.Println(year)
+	fmt.Println(temperature)
+}
