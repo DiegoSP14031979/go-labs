c5f9f3d435d88bc7caa204ffc763b252a00a6ec3
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-18 16:11:46 +0200

    Initialize variables for language and version
    Added variable declarations for language and version.

---
+++ 02-variables/02_inference.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Variables declaradas con tipos explícitos
+    var name string = "Gopher"
+    var age int = 5
+
+    // TODO: Reemplaza la línea de abajo con una declaración := de 'language', usando el valor que pide la tarea
+    language := "Go"
+
+    // TODO: Reemplaza la línea de abajo con una declaración := de 'version', usando el valor que pide la tarea
+    version := 1.18
+
+    // Imprime todas las variables
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Language:", language)
+    fmt.Println("Version:", version)
+}
