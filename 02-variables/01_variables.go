6f8b988663087247ab12c0e3ca1258302cd3d408
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-18 16:10:32 +0200

    Add variables for name and age in main function

---
+++ 02-variables/01_variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: Declara una variable llamada 'name' como string usando var, con el valor que pide la tarea
+    var name string = "Gopher"
+
+    // TODO: Declara una variable llamada 'age' como int usando var, con el valor que pide la tarea
+    var age int = 5
+
+    // No modifiques el código de abajo
+    fmt.Println("My name is", name, "and I am", age, "years old.")
+}
