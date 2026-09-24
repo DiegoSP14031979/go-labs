78b0cb3646c834c257cc2e5136b5d8f1ee1f91be
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-24 11:22:40 +0200

    06_string.go
    Add variable declarations for title, genre, rating, and theater.

---
+++ 02-variables/06_string.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: declara title usando var con un tipo string explícito
+    var title string = ""
+
+    // TODO: declara genre usando :=
+    genre := ""
+
+    // TODO: declara rating usando :=
+    rating := ""
+
+    // TODO: declara theater usando var con un tipo string explícito
+    var theater string = ""
+
+    fmt.Println(title)
+    fmt.Println(genre)
+    fmt.Println(rating)
+    fmt.Println(theater)
+}
