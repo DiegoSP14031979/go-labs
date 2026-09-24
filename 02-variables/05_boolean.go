933f56269f91bd527fb404efcfb2d0a53c082615
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-24 11:19:19 +0200

    05_boolean.go

---
+++ 02-variables/05_boolean.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: declara isOnline usando var con un tipo bool explícito, valor true
+    var isOnline bool = false
+
+    // TODO: declara hasShield usando :=, valor false
+    hasShield := true
+
+    // TODO: declara isPoisoned usando :=, valor true
+    isPoisoned := false
+
+    // TODO: declara isAdmin usando var con un tipo bool explícito, valor false
+    var isAdmin bool = true
+
+    fmt.Println("Online:", isOnline)
+    fmt.Println("Shield:", hasShield)
+    fmt.Println("Poisoned:", isPoisoned)
+    fmt.Println("Admin:", isAdmin)
+}
