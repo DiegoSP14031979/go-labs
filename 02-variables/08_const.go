ae5aaa5198aadebd14eabbb0c5d5de4335e8816f
Diego Santiago Pajares <dsantiagopajares@gmail.com>
2026-09-26 09:15:44 +0200

    08_const.go

---
+++ 02-variables/08_const.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: declara una constante appName con el valor "TaskManager"
+    const appName = "TaskManager"
+
+    // TODO: declara una constante maxUsers con el valor 100
+    const maxUsers = 100
+
+    // TODO: declara una constante version con un tipo float64 explícito, valor 2.5
+    const version float64 = 2.5
+
+    fmt.Println("App:", appName)
+    fmt.Println("Max users:", maxUsers)
+    fmt.Println("Version:", version)
+}
